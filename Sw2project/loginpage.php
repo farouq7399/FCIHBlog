c752f2ac45bcfef18f3e0c2289166d0fee4906bd
unknown <bluhanfi@gmail.com>
2020-03-10 08:36:18 +0200

    trying to login page

--- Sw2project/loginpage.php
+++ Sw2project/loginpage.php
@@ -1,7 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html>
+<body>
 
-echo "done right!";
-#test number two
-#test 33333333
-#test abdohanfi
-?>
+  <form method="post" action="loginpage.php">
+     username<br/>
+     <input type="text" name="username"><br/>
+     password<br/>
+     <input type="password" name="password"><br/>
+     <input type="submit" value="Login!">
+   </form>
+</body>
+</html>
