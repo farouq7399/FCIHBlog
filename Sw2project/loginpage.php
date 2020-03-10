be0d1e3024406ba08e8a165224a57333dff534b5
unknown <bluhanfi@gmail.com>
2020-03-10 09:02:32 +0200

    trying to login page without the db

--- Sw2project/loginpage.php
+++ Sw2project/loginpage.php
@@ -1,7 +1,25 @@
 <!DOCTYPE html>
 <html>
 <body>
+<?php
+$username="admin";
+$password="admin";
 
+session_start()
+if (isset($_SESSION['loggedin'])&& $_SESSION['loggedin']== true){
+
+header("location:hello.php")
+
+}
+if (isset($_POST['username']) && isset($_POST['password']) {
+   if($_POST['username']==$username && $_POST['password']==$password)
+   {
+     $_SESSION['loggedin']=true;
+     header("location:hello.php");
+   }
+}
+
+  ?>
   <form method="post" action="loginpage.php">
      username<br/>
      <input type="text" name="username"><br/>
@@ -9,5 +27,6 @@
      <input type="password" name="password"><br/>
      <input type="submit" value="Login!">
    </form>
+
 </body>
 </html>
